0a8bff12132c917beebbc8fde30371a8d9f034dc
Desmond <obend678@gmail.com>
2021-09-12 17:38:13 +0100

    working uploading report

---
+++ src/components/service/ReportersCard.tsx
@@ -0,0 +1,119 @@
+import { IonAvatar, IonBackdrop, IonBadge, IonButton, IonButtons, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonImg, IonItem, IonItemDivider, IonLabel, IonList, IonModal, IonNote, IonRow, IonSlide, IonSlides, IonText, IonToolbar } from "@ionic/react";
+import { add, banOutline, checkmarkDone, close, people, peopleOutline } from "ionicons/icons";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import LetterAvatar from "../LetterAvatar";
+import ReportStatistics from "./report-statistics";
+import ViewReportModal from "./ViewReportModal";
+import TimeAgo from '../timeago';
+import { reportInterface } from "../../interfaces/reportTypes";
+import { UserInterface } from "../../interfaces/users";
+import { selectUser } from "../../states/reducers/userReducers";
+import { useSelector } from "react-redux";
+import ReportersModal from "./ReportersModal";
+
+
+
+interface stylesInterface {
+    grid: React.CSSProperties
+}
+
+
+
+
+const styles: stylesInterface = {
+    grid: {
+
+    }
+}
+
+
+const ReportersCard: React.FC<{ report: reportInterface }> = ({ report }) => {
+    const [viewReport, setviewReport] = useState(false);
+    const user: UserInterface = useSelector(selectUser)
+    const [seen, setseen] = useState(false)
+    console.log(user.email, report.author);
+    useEffect(() => {
+        setseen((report.seenBy || []).filter(item => (item.code == `010001`)).length > 0);
+
+    }, [report])
+
+    return (
+        <div>
+            <IonItem disabled={!report.location && !report.author && report.images.length <= 0} onClick={() => setviewReport(true)}>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol className={`ion-align-self-center`}>
+                            {report.author && <>
+                                {report.photoUrl && <IonAvatar>
+                                    <IonImg src={report.photoUrl} />
+                                </IonAvatar>}
+                                {!report.photoUrl && <UserAvatar name={report.author} ></UserAvatar>}</>}
+                            {
+                                !report.author && <IonIcon size={`large`} icon={banOutline} />
+                            }
+                        </IonCol>
+                        <IonCol className={`ion-align-self-center ion-text-capitalize`} >
+                            <IonRow>
+                                <div >
+                                    <IonLabel>{report.author}</IonLabel>
+                                </div>
+                                <div style={{ height: `30px` }}></div>
+                                <div>
+                                    <IonLabel>{report.category}</IonLabel>
+                                </div>
+                            </IonRow>
+                            <IonRow>
+                                <IonNote>
+                                    {report.description}
+                                </IonNote>
+                            </IonRow>
+                        </IonCol>
+                        <IonCol className={`ion-align-self-center ion-text-center`}>
+                            <IonRow style={{ textAlign: `center` }}>
+                                {(!seen && user.email !== report.author) && <IonCol>
+                                    <IonBadge color={`success`}><div style={{ width: `4px`, height: `4px` }}></div> </IonBadge>
+                                    {/* <IonIcon color={`success`} icon={checkmarkDone}></IonIcon> */}
+                                </IonCol>}
+                            </IonRow>
+                            <IonRow className={`ion-text-center`}>
+                                <IonNote className={`ion-text-center`} style={{ textAlign: `center` }} color={`secondary`}>
+                                    <TimeAgo timestamp={report.timestamp}></TimeAgo>
+                                </IonNote>
+                            </IonRow>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonItem>
+            <ReportersModal report={report} onDidDismiss={() => setviewReport(false)} isOpen={viewReport}></ReportersModal>
+            {/* <ReportStatistics></ReportStatistics> */}
+        </div>
+    )
+}
+
+
+export default ReportersCard
+
+export const UserAvatar: React.FC<{ name: string }> = ({ name }) => {
+
+    const arrayWithColors = [
+        '#2ecc71',
+        '#3498db',
+        '#8e44ad',
+        '#e67e22',
+        '#e74c3c',
+        '#1abc9c',
+        '#2c3e50'
+    ];
+
+    return (
+        <LetterAvatar
+            name={name.trim()[0]}
+            size={57}
+            // radius={20}
+            color="#fff"
+            // backgroundColor="rgb(55,55,22)"
+            backgroundColors={arrayWithColors}
+        />
+    )
+}
---
+++ src/components/service/ReportersModal.tsx
@@ -0,0 +1,177 @@
+import { Toast } from '@capacitor/toast'
+import { IonModal, IonHeader, IonToolbar, IonButtons, IonBackdrop, IonButton, IonIcon, IonLabel, IonContent, IonCardContent, IonGrid, IonRow, IonCol, IonSlides, IonSlide, IonCardHeader, IonCardTitle, IonList, IonItemDivider, IonItem, IonSpinner, IonImg } from '@ionic/react'
+import { peopleOutline, close, checkmark, trash, trashOutline, share, shareSocial, starOutline } from 'ionicons/icons'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { fstore } from '../../Firebase/Firebase'
+import { markThisIncidentAsRead } from '../../Firebase/services/report'
+import { countryInfoInterface } from '../../interfaces/country'
+import { reportInterface } from '../../interfaces/reportTypes'
+import { UserInterface } from '../../interfaces/users'
+import { selectCountry } from '../../states/reducers/countryReducer'
+import { selectServiceAccount } from '../../states/reducers/service-reducer'
+import { selectUser } from '../../states/reducers/userReducers'
+import { UserAvatar } from './reportCard'
+import { accountInterface } from './serviceTypes'
+
+const ReportersModal: React.FC<{ isOpen: boolean, onDidDismiss: () => void, report: reportInterface }> = function ({ isOpen, onDidDismiss, report }) {
+    const [maploaded, setmaploaded] = useState(true)
+    const country: countryInfoInterface = useSelector(selectCountry);
+    const user: UserInterface = useSelector(selectUser);
+    const servAcc: accountInterface = useSelector(selectServiceAccount);
+    console.log(servAcc)
+
+    useEffect(() => {
+        if (servAcc && servAcc.code) {
+            if (user.email == report.author) return;
+            if (report.seenBy.filter(serv => serv.code == servAcc.code).length > 0) return;
+            markThisIncidentAsRead(report, servAcc, () => {
+
+            });
+
+        }
+    }, [])
+    async function deleteReport() {
+        const reportData = {
+            author: ``,
+            category: ``,
+            images: [],
+            country: report.country,
+            description: `Report id ${report.id} deleted  by ${user.name} | (${user.email}) from ${report.author}`,
+            id: report.id,
+            photoUrl: ``,
+            seenBy: [],
+            sentTo: [],
+            timestamp: Date.now()
+
+        }
+        onDidDismiss()
+        await fstore.collection(`users`).doc(`${user.email}`).collection(`reports`).doc(report.id).delete()
+        Toast.show({ text: `deleted...` });
+    }
+    return (
+        <IonModal isOpen={isOpen} onDidDismiss={onDidDismiss}>
+            <IonHeader>
+                <div style={{ height: `25px`, background: `var(--ion-color-primary)` }} className="status-bar"></div>
+                <IonToolbar color={`primary`} >
+                    <IonButtons slot={`start`}>
+                        <IonBackdrop></IonBackdrop>
+                        <IonButton>
+                            <IonIcon icon={close} />
+                        </IonButton>
+                    </IonButtons>
+                    <div className={`ion-text-capitalize ion-text-capitalize`}>
+                        <IonLabel style={{ textTransform: `capitalize` }} >
+                            Report Info
+               </IonLabel>
+                    </div>
+                    <IonButtons slot={`end`}>
+                        <IonButton onClick={deleteReport}>
+                            <IonIcon icon={trashOutline} />
+                        </IonButton>
+                        <IonButton >
+                            <IonIcon icon={starOutline} />
+                        </IonButton>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonCardContent>
+                    <IonToolbar   >
+                        <IonGrid>
+                            <IonRow>
+                                <IonCol className={`ion-align-self-start`}>
+                                    <UserAvatar name={`oBEN`}></UserAvatar>
+                                </IonCol>
+                                <IonCol className={`ion-align-self-center `}>
+                                    <IonLabel color={`secondary`}>
+                                        Report by <Link to={`#`}>@{report.author}</Link>
+                                    </IonLabel>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonToolbar>
+                    {/* <IonToolbar> */}
+                    <IonSlides options={{
+                        speed: 400,
+                        spaceBetween: 100,
+                    }} pager>
+                        {
+                            report.images.map((img, index) => <IonSlide key={index}>
+                                <IonImg src={img} />
+                            </IonSlide>)
+                        }
+                    </IonSlides>
+                    {/* </IonToolbar> */}
+                    <IonToolbar className={`ion-padding-vertical`}>
+                        <IonCardHeader>
+                            <IonCardTitle className={`ion-text-capitalize`} >
+                                {report.category}
+                            </IonCardTitle>
+                        </IonCardHeader>
+                        <p >
+                            {report.description}
+                        </p>
+
+
+                        <IonList className={`ion-padding-top ion-margin-top`}>
+                            {report.sentTo.length > 0 && <IonItemDivider>
+                                Sent To
+                    </IonItemDivider>}
+                            {
+                                report.sentTo.map((sento, index) => {
+                                    return (
+                                        <IonItem key={index}>
+                                            <IonIcon icon={peopleOutline} slot={`start`} />
+                                            <IonLabel>
+                                                {sento.name}
+                                            </IonLabel>
+                                        </IonItem>
+                                    )
+                                })
+                            }
+                            <div style={{ height: `30px` }}></div>
+                            {report.seenBy.length > 0 && <IonItemDivider>
+                                Viewed by
+                    </IonItemDivider>}
+                            {
+                                report.seenBy.map((seenby, index) => {
+                                    return (
+                                        <IonItem key={index}>
+                                            <IonIcon icon={peopleOutline} slot={`start`} />
+                                            <IonLabel>
+                                                {seenby.name}
+                                            </IonLabel>
+                                            <IonButton fill={`clear`} color={`success`}>
+                                                <IonIcon icon={checkmark} />
+                                            </IonButton>
+                                        </IonItem>
+                                    )
+                                })
+                            }
+
+                            <div style={{ height: `50px` }}></div>
+                            {maploaded && <div>
+                                <IonLabel color={`secondary`}>Location of Incident</IonLabel>
+                                {report?.location && <iframe onLoadStart={() => setmaploaded(false)} onLoadedData={() => setmaploaded(true)} onLoad={() => setmaploaded(true)} src={`http://maps.google.com/maps?q=${report.location.lat}, ${report.location}&z=11&output=embed`} height="450" style={{ border: "0", width: `100%` }} loading="lazy"></iframe>}
+                            </div>}
+                            {!maploaded && <IonGrid >
+                                <IonRow>
+                                    <IonCol></IonCol>
+                                    <IonCol style={{ textAlign: `center`, padding: `30px` }}>
+                                        <IonSpinner color={`secondary`}></IonSpinner>
+                                    </IonCol>
+                                    <IonCol></IonCol>
+                                </IonRow>
+                            </IonGrid>}
+                        </IonList>
+                    </IonToolbar>
+                </IonCardContent>
+            </IonContent>
+        </IonModal>
+    )
+}
+
+
+export default ReportersModal
