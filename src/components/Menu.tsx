8dbe5e674a6c7ee798058db2ff55203895d188b7
Desmond <obend678@gmail.com>
2021-09-13 15:53:37 +0100

    working uploading report

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -21,7 +21,7 @@ import {
 } from '@ionic/react';
 
 import { useHistory, useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, cashOutline, chevronDown, chevronUp, exit, exitOutline, flagOutline, heart, heartOutline, heartSharp, homeOutline, mailOutline, mailSharp, notificationsOutline, paperPlaneOutline, paperPlaneSharp, peopleOutline, settingsOutline, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import { archiveOutline, archiveSharp, bookmarkOutline, cashOutline, chevronDown, chevronUp, exit, exitOutline, flagOutline, heart, heartOutline, heartSharp, homeOutline, mailOutline, mailSharp, notificationsOutline, paperPlaneOutline, paperPlaneSharp, peopleOutline, personOutline, settingsOutline, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
 import './Menu.css';
 import { Pictures } from '../pages/images/images';
 import { useEffect, useState } from 'react';
@@ -65,6 +65,12 @@ const appPages: AppPage[] = [
     url: '/notifications',
     iosIcon: notificationsOutline,
     mdIcon: notificationsOutline
+  },
+  {
+    title: 'Profile',
+    url: '/profile',
+    iosIcon: personOutline,
+    mdIcon: personOutline
   },
   // {
   //   title: 'Countries',
