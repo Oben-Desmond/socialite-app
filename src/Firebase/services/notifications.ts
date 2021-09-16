39ce20443bfa8a521e6e1caec011099b634e5026
Desmond <obend678@gmail.com>
2021-09-16 01:24:11 +0100

    working on notifications

--- src/Firebase/services/notifications.ts
+++ src/Firebase/services/notifications.ts
@@ -1,9 +1,65 @@
-async function sendNotification() {
-    db.ref('tokens').child('obend678@gmailcom').once('value', (snapshot) => {
+import axios from "axios"
+import { db } from "../Firebase"
+
+export interface NotificationPayload {
+    body: string,
+    title: string,
+    image: string
+}
+
+
+export interface NotificationData {
+    type: string,
+    id: string
+}
+
+
+async function sendNotification(param: { notification: NotificationPayload, data: NotificationData, email: string }) {
+    const { data, notification, email } = param;
+
+    db.ref('tokens').child(email).once('value', (snapshot) => {
         const token = snapshot.val()
         if (token) {
-            axios.post('https://socialiteapp-backend.herokuapp.com/message/single', { token }).catch(alert).then(alert)
+            axios.post('https://socialiteapp-backend.herokuapp.com/message/single', { token, data, notification }).catch(alert).then(alert)
         }
     })//
 
 }
+
+
+
+async function sendManyNotifications(param: { notification: NotificationPayload, data: NotificationData, emails: string[] }) {
+    const { data, notification, emails } = param;
+
+    const tokens = await getTokens(emails)
+
+}
+
+
+async function getTokens(emails: string[]) {
+
+    try {
+        const queryTokens = emails.map((email) => {
+            return (new Promise((resolve, reject) => {
+                db.ref('tokens').child(email).once('value', (snapshot) => {
+                    const token = snapshot.val()
+                    if (token) {
+                        resolve(token)
+                    }
+                    else {
+                        resolve('')
+                    }
+                })
+            }))
+        })
+        return Promise.all(queryTokens).then((res) => {
+            return res.filter(token => (token && token != ''));
+        })
+    } catch (err) {
+        alert(err.message || err)
+    }
+
+    return [];
+
+}
+// axios.post('https://socialiteapp-backend.herokuapp.com/message/single', { token, data, notification }).catch(alert).then(alert)
