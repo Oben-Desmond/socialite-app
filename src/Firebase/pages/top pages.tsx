09f9f2c696b55698a57b8f376c8b9892cb8f290f
Desmond <obend678@gmail.com>
2021-09-28 08:32:50 +0100

    working with maps

--- src/Firebase/pages/top pages.tsx
+++ src/Firebase/pages/top pages.tsx
@@ -130,4 +130,54 @@ export function fetchPostById(id: string, country: string, callBack: (val: PostI
 
 }
 
+
+export async function getSyncedFeed(distance: number, country: string, location:{long:number, lat:number}) {
+  
+    // Find cities within 50km of London
+    const center = [location.lat, location.long];
+    const radiusInM = distance;
+
+    // Each item in 'bounds' represents a startAt/endAt pair. We have to issue
+    // a separate query for each pair. There can be up to 9 pairs of bounds
+    // depending on overlap, but in most cases there are 4.
+    const bounds = geofire.geohashQueryBounds(center, radiusInM);
+    const promises = [];
+    for (const b of bounds) {
+        const q =  fstore.collection(`posts`).doc(country).collection(`feed`)
+            .orderBy('geohash')
+            .startAt(b[0])
+            .endAt(b[1]);
+
+        promises.push(q.get());
+    }
+
+    // Collect all the query results together into a single list
+    Promise.all(promises).then((snapshots) => {
+        const matchingDocs = [];
+
+        for (const snap of snapshots) {
+            for (const doc of snap.docs) {
+                const lat = doc.get('coords')[0];
+                const lng = doc.get('coords')[1];
+
+                // We have to filter out a few false positives due to GeoHash
+                // accuracy, but most will match
+                const distanceInKm = geofire.distanceBetween([lat, lng], center);
+                const distanceInM = distanceInKm * 1000;
+                if (distanceInM <= radiusInM) {
+                    matchingDocs.push(doc);
+                }
+            }
+        }
+
+        return matchingDocs;
+    }).then((matchingDocs) => {
+        // Process the matching documents
+        // ...
+    });
+
+
+}
+
+
 export { };
