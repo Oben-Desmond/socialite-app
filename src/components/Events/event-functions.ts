d47a7b405b27e29dea7b8a4fb7e337bdb08c22d0
Desmond <obend678@gmail.com>
2021-09-29 14:18:19 +0100

    working with maps

--- src/components/Events/event-functions.ts
+++ src/components/Events/event-functions.ts
@@ -7,12 +7,14 @@ import { PostInterface } from "../../interfaces/posts";
 
 
 export function fetchEventById(id:string, country:string, callBack: (val:PostInterface|any)=>void, failed:(err:any)=>void){
-     
+     alert(id+`<-- id `)
+     alert(country+`<-- country `)
     fstore.collection(`posts`).doc(country).collection(`events`).doc(id)
     .get().then(snapshot=>{
+        alert(JSON.stringify(snapshot.data()))
         if(snapshot.data()){
             callBack(snapshot.data());
-            return
+            return;
         }
         failed({message:`no data`})
         Dialog.alert({title:`Error getting Post`,message:`Post does not Exist. it may have been deleted`})
